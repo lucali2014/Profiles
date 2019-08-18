df688dbdfe43be8785fb52c279eda64feafd5ff0
cyan <54043726+Cyansx@users.noreply.github.com>
2019-08-18 21:20:52 +0800

    Update zhihuRecommed.js

--- Surge/Script/zhihuRecommed.js
+++ Surge/Script/zhihuRecommed.js
@@ -1,8 +1,9 @@
 /*by onewayticket25
 
-http-response https://app.bilibili.com/x/resource/show/tab requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/Cyansx/Profiles/master/Surge/Script/zhihuRecommed.js,script-update-interval=-1
+http-response https://api.zhihu.com/topstory/recommend requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/Cyansx/Profiles/master/Surge/Script/zhihuRecommed.js,script-update-interval=-1
 
 hostname = app.zhihu.com
+
 
 */
 let body = $response.body
