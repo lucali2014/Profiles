aa1c2d19bed3705adaf3f3d3abd026fe4bb6f10a
cyan <54043726+Cyansx@users.noreply.github.com>
2019-08-18 20:48:16 +0800

    Create bilibiliAccount.js

---
+++ Surge/Script/bilibiliAccount.js
@@ -0,0 +1,18 @@
+/*by onewayticket25
+
+http-response https://app.bilibili.com/x/resource/show/tab requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/Cyansx/Profiles/master/Surge/Script/bilibiliAccount.js,script-update-interval=-1
+
+hostname = app.bilibili.com, api.bilibili.com
+
+*/
+
+
+
+let body = $response.body
+body=JSON.parse(body)
+body['data']['sections'].splice(0,1)
+body['data']['sections'][0]['items'].splice(3,1)
+body['data']['sections'][0]['items'].splice(4,3)
+body['data']['sections'].splice(1,1)
+body=JSON.stringify(body)
+$done({body})
