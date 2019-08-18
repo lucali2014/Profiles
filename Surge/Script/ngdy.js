29c287c4e73ab9fe328c884ae25f916ef9b239c1
cyan <54043726+Cyansx@users.noreply.github.com>
2019-08-18 20:07:37 +0800

    Update and rename nanguayingshi.js to ngdy.js

--- Surge/Script/ngdy.js
+++ Surge/Script/ngdy.js
@@ -1,7 +1,7 @@
 /* Made by Meeta(南瓜电影)
 使用方法：
 1.文本编辑模式下复制粘贴
-http-response https?:\/\/p\.doras\.api\.vcinema\.cn\/v5.0\/user/  script-path= https://raw.githubusercontent.com/Cyansx/Profiles/master/Surge/Script/nanguayingshi.js,requires-body=true
+http-response https?:\/\/p\.doras\.api\.vcinema\.cn\/v5.0\/user/  script-path= https://raw.githubusercontent.com/Cyansx/Profiles/master/Surge/Script/ngdy.js,requires-body=true
 2.添加hostname = *.api.vcinema.cn
 3.南瓜电影登录账号
 */
