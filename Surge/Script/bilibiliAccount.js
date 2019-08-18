44f8afc055b6d96904ea6d8f045fcc2a991826c4
cyan <54043726+Cyansx@users.noreply.github.com>
2019-08-18 21:28:03 +0800

    Update bilibiliAccount.js

--- Surge/Script/bilibiliAccount.js
+++ Surge/Script/bilibiliAccount.js
@@ -1,6 +1,6 @@
 /*by onewayticket25
 
-http-response https://app.bilibili.com/x/resource/show/tab requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/Cyansx/Profiles/master/Surge/Script/bilibiliAccount.js,script-update-interval=-1
+http-response https://app.bilibili.com/x/v2/account/mine requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/Cyansx/Profiles/master/Surge/Script/bilibiliAccount.js,script-update-interval=-1
 
 hostname = app.bilibili.com, api.bilibili.com
 
