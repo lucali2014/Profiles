95bfbf966d9ae256fb68b298e41c5ddf32c305f4
cyan <54043726+Cyansx@users.noreply.github.com>
2019-08-18 21:30:12 +0800

    Update bilibiliRank.js

--- Surge/Script/bilibiliRank.js
+++ Surge/Script/bilibiliRank.js
@@ -1,6 +1,6 @@
 /*by onewayticket25
 
-http-response https://app.bilibili.com/x/resource/show/tab requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/Cyansx/Profiles/master/Surge/Script/bilibiliRank.js,script-update-interval=-1
+http-response https://app.bilibili.com/x/v2/rank requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/Cyansx/Profiles/master/Surge/Script/bilibiliRank.js,script-update-interval=-1
 
 hostname = app.bilibili.com, api.bilibili.com
 
