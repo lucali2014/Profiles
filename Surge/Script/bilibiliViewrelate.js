2328854b9ddedfa0cc4616f6d489aca2202833fe
cyan <54043726+Cyansx@users.noreply.github.com>
2019-08-25 23:16:45 +0800

    Update and rename bilibiliViewlate.js to bilibiliViewrelate.js

--- Surge/Script/bilibiliViewrelate.js
+++ Surge/Script/bilibiliViewrelate.js
@@ -2,7 +2,7 @@
 多了|| element.hasOwnProperty('from') by小壮提供
 
 
-http-response https://app.bilibili.com/x/v2/view\?access_key requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/Cyansx/Profiles/master/Surge/Script/bilibiliViewlate.js,script-update-interval=-1
+http-response https://app.bilibili.com/x/v2/view\?access_key requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/Cyansx/Profiles/master/Surge/Script/bilibiliViewrelate.js,script-update-interval=-1
 
 hostname = app.bilibili.com, api.bilibili.com
 
