1b2f56a7a2a97fb77e7477014bca40e598175fbe
cyan <54043726+Cyansx@users.noreply.github.com>
2019-08-26 22:38:00 +0800

    Update WeChat.js

--- Script/WeChat.js
+++ Script/WeChat.js
@@ -5,7 +5,10 @@ http-response ^https://mp\.weixin\.qq\.com/mp/getappmsgad requires-body=1,max-si
 hostname = mp.weixin.qq.com
 */
 
-var obj = JSON.parse($response.body);
-obj.advertisement_num = 0;
-obj.advertisement_info = [];
-$done({body: JSON.stringify(obj)});
+var data = {
+  body: "{}",
+  headers: {
+    "Content-Type": "application/json"
+  }
+};
+$done({response: data});
