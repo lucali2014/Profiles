90cc25b6d3611992fce29195026bac64268b4707
cyan <54043726+Cyansx@users.noreply.github.com>
2019-08-26 22:15:40 +0800

    Update zhihuAnswer.js

--- Surge/Script/zhihuAnswer.js
+++ Surge/Script/zhihuAnswer.js
@@ -1,10 +1,10 @@
-/*by onewayticket25
+//by onewayticket25
 
-http-response https://api.zhihu.com/.*/questions requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/Cyansx/Profiles/master/Surge/Script/zhihuAnswer.js,script-update-interval=-1
+//http-response https://api.zhihu.com/.*/questions requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/Cyansx/Profiles/master/Surge/Script/zhihuAnswer.js,script-update-interval=-1
 
-hostname = app.zhihu.com
+//hostname = app.zhihu.com
 
-*/
+
 
 let body = $response.body
 body=JSON.parse(body)
